516405a5496c4fbbc151ada35179872a7524a0dc
smajkelegenda <108105528+smajkelegenda@users.noreply.github.com>
2022-06-24 04:46:21 +0200

    Add files via upload

--- shell.c
+++ shell.c
@@ -97,6 +97,22 @@ void prompt(){
 
 }
 
+int isDir(char* PATH){
+    /* Returns
+            0 -- PATH is not present
+            1 -- PATH is a directory
+            2 -- PATH is a regular file
+    */
+    struct stat check_file; // some other name please
+    if (stat(PATH, &check_file)==-1){ // 0 when file exist
+        // printf("cannot stat on %s location!\n", PATH);  //are you planning to print this?
+        return 0;
+    }
+    if (S_ISDIR(check_file.st_mode)) return 1;
+    if (S_ISREG(check_file.st_mode)) return 2;
+
+}
+
 
 void mv(char oldpath[MAX_FILE_NAME_LENGTH], char dst[MAX_FILE_NAME_LENGTH]){
     /* moves oldpath to dst */
