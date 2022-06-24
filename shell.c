0eb838ceb8be586ba166c035a6059a59d222a55e
smajkelegenda <108105528+smajkelegenda@users.noreply.github.com>
2022-06-24 13:01:07 +0200

    Add files via upload

--- shell.c
+++ shell.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -11,19 +12,7 @@
 #include <libgen.h>
 #include <signal.h>
 #include <sys/wait.h>
-
-#include <stdio.h>
-#include <fcntl.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <errno.h>
-#include <libgen.h>
-#include "isDir.h"
-
-#define MAX_FILE_NAME_LENGTH 1024
+#include <time.h>
 
 int loop = 1;
 
@@ -97,320 +86,128 @@ void prompt(){
 
 }
 
-int isDir(char* PATH){
-    /* Returns
-            0 -- PATH is not present
-            1 -- PATH is a directory
-            2 -- PATH is a regular file
-    */
-    struct stat check_file; // some other name please
-    if (stat(PATH, &check_file)==-1){ // 0 when file exist
-        // printf("cannot stat on %s location!\n", PATH);  //are you planning to print this?
+
+
+void date() {
+	time_t T = time(NULL);
+    struct tm tm = *localtime(&T);
+
+    printf("System Date is: %02d/%02d/%04d\n", tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900);
+    printf("System Time is: %02d:%02d:%02d\n", tm.tm_hour, tm.tm_min, tm.tm_sec);
+}
+
+
+void rev(char file_name[128]) {
+	FILE *fp;
+    char ch, newch[500];
+    int i=0, j, COUNT=0;
+     
+    fp = fopen(file_name, "r");
+    if(!fp)
+    {
+        printf("Error in opening the file...\nExiting...");
+        getch();
         return 0;
     }
-    if (S_ISDIR(check_file.st_mode)) return 1;
-    if (S_ISREG(check_file.st_mode)) return 2;
-
-}
-
-
-void mv(char oldpath[MAX_FILE_NAME_LENGTH], char dst[MAX_FILE_NAME_LENGTH]){
-    /* moves oldpath to dst */
-    if (isDir(oldpath) == 0){ //oldpath does not exist
-        // handling error
-        fprintf(stderr, "mv: '%s': %s\n", oldpath, strerror(errno));
-        return;
-    }
-
-    int check_type = isDir(oldpath);
-    char filename[MAX_FILE_NAME_LENGTH]="";
-    // get the source filename
-    if (check_type == 1){
-        strcpy(filename, basename(oldpath));
-    }
-    else{
-        strcpy(filename, oldpath);
-    }
-
-    // destination path
-    char newPath[MAX_FILE_NAME_LENGTH] = "";
-    strncpy(newPath, dst, strlen(dst));
-    // update destination path if dst is a directory.
-    if (isDir(newPath) == 1){
-        // adds suffix "/filename" to the directory name
-        strcat(newPath, "/");
-        strcat(newPath, filename);
-    }
-    int ret = rename(oldpath, newPath);
-    if (ret == -1){
-        // handling error
-        fprintf(stderr, "mv: %s\n", strerror(errno));
-        return;
-    }
-}
-
-void mkdir_c(char flags[10], int f_size, char name[128], int n_size){
-
-	char dir[PATH_MAX]; // PATH_MAX - defines the longest path a direcotry can be
-	getcwd(dir, sizeof(dir)); // getcwd - gives us the absolute pathname of the current working directory
-
-	char slash[129] = "/"; // creates a new string slighty bigger than name
-	strcat(slash,name); // in order to concatenate res -> /name_of_our_new_dir
-	slash[strcspn(slash, "\n")] = 0;
-
-	if(name[0] != '\n'){ // prevents creating a '' named directory
-
-		mkdir(strcat(dir,slash), 0777); // make the dir with mkdir function
-		// 0777 -> -rwxrwxrwx read,write & execute for owner, group and others
-
-	}
-
-	/**
-	 * The user can choose to input many flags, so we go throught the flags array
-	 * detecting the user flags and executing them.
-	 */
-
-	for( int i = 0;i < f_size;i++){
-		if(flags[i] == 'v'){
-			name[strlen(name)-1] = '\'';
-			printf("mkdir: created directory '%s\n", name);
-		}
-		if(flags[i] == 'h'){
-			printf("Create the DIRECTORY, if they do not already exist\n");
-			printf("Usage mkdir [OPTION] ... DIRECTORY...\n");
-			printf("-v print a message for a created directory\n");
-			printf("-V print the current version\n");
-			printf("-h show the help page\n");
-		}
-		if(flags[i] == 'V'){
-			printf("current version mkdir : v0.0.1\n");
-		}
-	}
-}
-
-void ls(char flags[10], int f_size){
-
-	// opendir() is included in dirent.h and opens a directory stream specified in the brackets
-	// in this case the "." which represents the current directory
-	DIR* dir = opendir(".");
-
-	// checking if the directory exists
-	if(dir == NULL){
-		return;
-	}
-	
-	struct dirent* entity;
-	entity = readdir(dir); // reading the contents of the directory
-	
-	while(entity != NULL){
-		if(f_size == 0){ // if no flags have been entered
-		printf("%s\n", entity->d_name); // printing the names (d_name) of files and directories in the current directory 
-		entity = readdir(dir); // reading the next file/directory 
-		}
-
-		/*
-		* -c (color) flag
-		* checks if the entity type is directory or file and changes the output color accordingly
-		* directories are purple, files are green
-		*/
-		for(int i = 0; i < f_size; i++){
-			if(flags[i] == 'c'){
-				// checking if entity type (d_type) is a directory (DT_DIR)
-				if(entity->d_type == DT_DIR){ 
-					purple(); // changes color to purple
-					printf("%s\n", entity->d_name); // prints the entity
-					reset(); // resets text to default color
-				}
-
-				else if(entity->d_type == DT_REG){ // checking if entity type (d_type) is a file (DT_REG)
-					green();
-					printf("%s\n", entity->d_name);
-					reset();	
-				}
-			}
-		entity = readdir(dir); // reading the contents of the directory
-		}
-	}
-	closedir(dir); // closes the directory stream
-
-	// more flags
-	for(int i = 0; i < f_size; i++){
-		if(flags[i] == 'h'){
-			printf("List FILEs in current directory\n");
-			printf("Usage ls [OPTION]...\n");
-			printf("-c add color to the output (directories - purple, files - green\n");
-			printf("-V print the current version\n");
-			printf("-h show the help page\n");
-		}
-		if(flags[i] == 'V'){
-			printf("current version ls : v0.0.1\n");
-		}
-	}
-}
-
-void uptime(char flags[10], int f_size, char file_name[128], int output){
-	
-	struct sysinfo s_info; // sysinfo() returns information on overall system statistics
-    int error = sysinfo(&s_info);
-
-	// on success sysinfo() returns 0 
-	// on error, -1 is returned
-	if(error != 0)
+    printf("\nThe original content is:\n\n");
+    ch = getc(fp);
+    while(ch != EOF)
     {
-        printf("code error = %d\n", error);
+        COUNT++;
+        putchar(ch);
+        newch[i] = ch;
+        i++;
+        ch = getc(fp);
     }
+    printf("\n\n\n");
+    printf("The content in reverse order is:\n\n");
+    for(j=(COUNT-1); j>=0; j--)
+    {
+        ch = newch[j];
+        printf("%c", ch);
+    }
+    printf("\n");
+    getch();
+}
 
-	/* s_info.uptime is system uptime in seconds
-	* then some formulas are used to convert seconds into hours, minutes and seconds
-	*/
-    int secs = s_info.uptime;
-    int hours = (int)(secs/3600);
-    int minutes = ((int)secs/60) % 60;
-	int seconds = (int)(secs%60);
+void mv(char file_name1[128], char file_name2[128])
+{
+	int i,fd1,fd2;
+	char buf[2];
+	file1=file_name1;
+	file2=file_name2;
+	printf("file1=%s file2=%s",file1,file2);
+	fd1=open(file1,O_RDONLY,0777);
+	fd2=creat(file2,0777);
+	while(i=read(fd1,buf,1)>0)
+	write(fd2,buf,1);
+	remove(file1);
+	close(fd1);
+	close(fd2);
+}
 
+void waitpid() {
+	int pid;
+    int status;
+    pid =fork();
+    if(pid<0){
+        printf("Error");
+    }
+    else if(pid==0){
+         // Child Process
+         for(int i=0;i<100;i++){
+            value-=1;
+            printf("%d \n",value);
+         }
+        exit(EXIT_SUCCESS);
+    }
 	
-	if(output == 0 && f_size == 0){ // if redirecting to file and flags are not used
-		printf("uptime: %d:%d:%d\n", hours, minutes, seconds);
-	}
-
-	if(f_size == 0){
-		if(output == 1){ // if we detected a '>' char in input we need to redirect the output
-			// first we create a pointer to FILE type
-			FILE * file;
-			// we need to remove the end line char from the file_name string
-			file_name[strcspn(file_name, "\n")] = 0;
-			// we open/create a new file and set the mode to write - 'w'
-			file = fopen(file_name,"w");
-
-			if(file == NULL){ // if the file was not created successfully we exit the function
-				return;
-			}
-			// we output the following string to file
-			fprintf(file,"uptime: %d:%d:%d\n", hours, minutes, seconds);
-			// we close the file
-			fclose(file);
-			// provide user with confirmation message
-			printf("Uptime written to a file : %s\n", file_name);
-		}
-
-	}
-
-	// flags
-	for(int i = 0; i < f_size; i++){
-
-		// -p (pretty) flag changes the color of the uptime command and makes the output easily readable
-		if(flags[i] == 'p'){
-			red();
-			printf("uptime: ");
-			yellow();
-			printf("%d", hours);
-			printf(" hours, ");
-			green();
-			printf("%d", minutes);
-			printf(" minutes, ");
-			purple();
-			printf("%d", seconds);
-			printf(" seconds\n");
-		}
-
-		if(flags[i] == 'V'){
-			printf("current version uptime : v0.0.1\n");
-		}
-		if(flags[i] == 'h'){
-			printf("Tell how long the system has been running\n");
-			printf("Usage uptime [OPTION]...\n");
-			printf("-p show uptime in pretty format\n");
-			printf("-V print the current version\n");
-			printf("-h show the help page\n");
-		}
+   waitpid(pid,&status,0); // WAIT till child gets over
+	// Parent process
+   for(int i=0;i<100;i++){
+            value-=1;
+            printf("%d \n",value);
+         }
+   printingFunction();
+    if (WIFSIGNALED(status)){
+        printf("Error\n");
+    }
+    else if (WEXITSTATUS(status)){
+        printf("Exited Normally\n");
+    }
+    return 0;
+    
 	}
 }
 
-void printline(char *line, int number){
-        for(int i = 0; i < number; i++){
-                printf(" "); // prints an empty space based on the value of number (space)
+void execvpe() {
+	char* command = "ls";
+    char* argument_list[] = {"ls", "-l", NULL};
+ 
+    printf("Before calling execvp()\n");
+ 
+    printf("Creating another process using fork()...\n");
+ 
+    if (fork() == 0) {
+        // Newly spawned child Process. This will be taken over by "ls -l"
+        int status_code = execvp(command, argument_list);
+ 
+        printf("ls -l has taken control of this child process. This won't execute unless it terminates abnormally!\n");
+ 
+        if (status_code == -1) {
+            printf("Terminated Incorrectly\n");
+            return 1;
         }
-        printf("%s\n", line); // prints a "line" of the car
+    }
+    else {
+        // Old Parent process. The C program will come here
+        printf("This line will be printed\n");
+    }
+ 
+    return 0;
 }
 
-// int spaces determines how far the car moves
-void sl(int spaces){ 
-	green();
 
-	// car is seperated in four lines
-	char *line1 = "       .--------.";
-	char *line2 = " ____/_____|___ \\___";
-	char *line3 = " O    _   - |   _   ,*";
-	char *line4 = " '--(_)-------(_)--'";
-
-	// prints all four segments of the car using printline() 
-	printline(line1, spaces);
-	printline(line2, spaces);
-	printline(line3, spaces);
-	printline(line4, spaces);
-	reset();
-}
-
-void fork_c(){
-
-	int rc = fork();
-	
-	if(rc != 0 && rc != -1){
-		printf("I am a parent waiting for my child to wake up\n");
-		wait(NULL);
-		printf("I am a parent and my child has woken up\n");
-	}
-	if(rc == 0){
-		/*
-		* The first argument is the location
-		* The second argument is the actual arguments which go to the main function of sleep.c
-		*/
-		/*
-		 * Usually the exec family of functions, overwrites the currently executing program, and does not return
-		 * to the caller program. But because we are executing the execl function on top of a child process
-		 * the child gets overwriten and we return to our parent (init) process and continue the execution of our shell
-		 */
-
-		execl("./sleep", "./sleep", NULL );
-
-		kill(getpid(), SIGINT); 
-		/*
-		* We have to make the child process kill itself, because if we don't, the child process will never end
-		* And the parent will wait forever. 
-		* if we dont kill the child process it will finish this function, finish in the router function
-		* and at last it will finish in the while(1) in the main function
-		* effectively having two processes, where one does not do anything and eats up memory.
-		*/
-	}
-}
-
-void vfork_c(){
-
-	int shared_variable = 100;
-	
-	/***
-	 * In vfork the child process halts the parent process until the child is done executing
-	 * In vfork the child process has the same memory scope as the parent process
-	 * The changes done in the child process will reflect the parent's memory
-	 * Here we have declared the variable shared_variable = 100
-	 * The child process increments it by one, then the child process is killed same reason as for fork_c()
-	 * The changes are reflected in the parent process memory space.
-	 */
-	int pid_id = vfork();
-
-	if(pid_id == 0){
-
-		printf("I am a child and I share parent's variables\n");
-		printf("Our variable before modifying in child process : %d\n", shared_variable);
-		shared_variable++;
-		printf("Our variable after modifying in child process : %d\n", shared_variable);
-
-		kill(getpid(), SIGINT);
-
-	}else{
-		printf("I am a parent and the variable is now : %d\n", shared_variable);
-	}
-}
 
 void forkbomb(){
 	/**
@@ -456,13 +253,13 @@ void forkbomb(){
  * Function used for routing user input, it can be though as the "heart" of the shell, since it
  * reads the user input, intreprets it and calls the wanted function.
 */
-void router(int argc, char* argv[]){
+void router(char input[1024]){
 
-
-    char function[10] = "";
+	char function[10] = "";
 	char flags[10] = "";
 	int output = 0;
 	char file_name[128] = "";
+	char file_name2[128] = "";
 	int fun_counter = 0;
 	int flag_counter = 0;
 	int name_counter = 0;
@@ -508,38 +305,18 @@ void router(int argc, char* argv[]){
 	// removes the '\n' char that fgets adds
 	function[strcspn(function, "\n")] = 0;
 
-	// simple if statements which calls the function to its desired input
-	
-	if(strcmp(function, "mkdir") ==  0){
-		mkdir_c(flags, flag_counter, file_name, name_counter);
-	}
-    
-    if(strcmp(function, "mv") ==  0){
-		char* dst= argv[argc-1]; // dst_dir
-        char pathname[MAX_FILE_NAME_LENGTH] = ""; // source path
-        for (int i=1; i<argc-1; i++){
-            strncpy(pathname, argv[i], strlen(argv[i]));
-            // move from pathname to dst
-            mv(pathname, dst);
-        }
+	else if(strcmp(function, "date") == 0){
+		date();
 	}
 
-	else if(strcmp(function, "ls") == 0){
-		ls(flags, flag_counter);
+	else if(strcmp(function, "rev") == 0){
+		rev(file_name);
 	}
 
-	else if(strcmp(function, "uptime") == 0){
-		uptime(flags, flag_counter, file_name, output);
+	else if(strcmp(function, "mv") == 0){
+		mv(file_name, file_name2);
 	}
 
-	else if(strcmp(function, "sl") == 0){
-		for (int i = 0; i < 110; i++){ 
-		system("sleep 0.01"); // movement speed of the car
-		system("clear"); // clears the screen before each instance of car appears
-		sl(i); // the car moves 110 spaces
-		}
-		system("clear");
-	}
 
 	else if(strcmp(function, "clear") == 0){
 		system("clear");
@@ -550,12 +327,12 @@ void router(int argc, char* argv[]){
 
 	} 
 	
-	else if(strcmp(function,"fork") == 0){
-		fork_c();
+	else if(strcmp(function,"waitpid") == 0){
+		waitpid();
 	}
 
-	else if(strcmp(function,"vfork") == 0){
-		vfork_c();
+	else if(strcmp(function,"execvpe") == 0){
+		execvpe();
 	}
 
 	else if(strcmp(function,"forkbomb") == 0){
@@ -568,10 +345,6 @@ void router(int argc, char* argv[]){
 }
 int main(void){
 
-	printf("%c]0;%s%c", '\033', "Medo", '\007');
-	/*
-	* The system("clear") is included in stdlib.h and works only in Linux based systems
-	*/
 
 	system("clear");
 	/*
@@ -592,4 +365,15 @@ int main(void){
 	}
 
 	return 0;
-}
+}Task 1.5:
+
+/*
+The purpose of fork() is to create a new process, which becomes the child process of the caller. After a new child process is created, both processes will execute the next instruction following the fork() system call.
+int i = 1; 
+while (i < 100) { i++; } 
+printf("%d ", i); 
+while (i > 0) { i--; }
+printf("%d ", i);
+In this code, process goes from executing 1st while loop and then terminating it. During this period 2nd while loop is ready and is waiting to start executing, when first loop is terminated then second one begins until condition is fulfilled. During this time first loop is terminated, after second loop is done it’s also terminated.
+
+*/
