9a5ed1dd988a6c88767342110767eeb7213b1311
smajkelegenda <108105528+smajkelegenda@users.noreply.github.com>
2022-06-24 03:09:04 +0200

    Add files via upload

--- shell.c
+++ shell.c
@@ -11,6 +11,21 @@
 #include <libgen.h>
 #include <signal.h>
 #include <sys/wait.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <ustat.h>
+#include <time.h>
+#include <utime.h>
+#include <grp.h>
+#include <sys/utsname.h>
+#include <limits.h>
 
 
 int loop = 1;
